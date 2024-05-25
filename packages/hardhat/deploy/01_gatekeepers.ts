874f6a88d9f38db2128437ce151c871f9c3ed670
gasperpre <49305355+gasperpre@users.noreply.github.com>
2024-05-25 15:45:55 +0200

    zupass gatekeeper

--- packages/hardhat/deploy/01_gatekeepers.ts
+++ packages/hardhat/deploy/01_gatekeepers.ts
@@ -5,9 +5,30 @@ import { GatekeeperContractName } from "../constants";
 const deployContracts: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
 
+  let args: string[] = [];
+
+  if (GatekeeperContractName === "ZupassGatekeeper") {
+    await hre.deployments.deploy("Groth16Verifier", {
+      from: deployer,
+      args: [],
+      log: true,
+      autoMine: true,
+    });
+
+    const verifier = await hre.ethers.getContract("Groth16Verifier", deployer);
+    const verifierAddress = await verifier.getAddress();
+    console.log(`The Groth16Verifier is deployed at ${verifierAddress}`);
+    args = [
+      "111560146890584288369567824893314450802", // ETHBerlin event ID
+      "13908133709081944902758389525983124100292637002438232157513257158004852609027", // ETHBerlin event signer[0]
+      "7654374482676219729919246464135900991450848628968334062174564799457623790084", // ETHBerlin event signer[1]+
+      verifierAddress,
+    ];
+  }
+
   await hre.deployments.deploy(GatekeeperContractName, {
     from: deployer,
-    args: [],
+    args,
     log: true,
     autoMine: true,
   });
