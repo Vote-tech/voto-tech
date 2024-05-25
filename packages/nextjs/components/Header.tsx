8a9eae4c9d460e2a291d8283660215bb56075249
Dovydas Musulas <dovydas.musulas@proton.me>
2024-05-25 12:31:52 +0200

    init

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -74,7 +74,7 @@ export const Header = () => {
   );
 
   return (
-    <div className="sticky lg:static top-0 navbar bg-base-100 min-h-0 flex-shrink-0 justify-between z-20 shadow-md shadow-secondary px-0 sm:px-2">
+    <div className="sticky lg:static top-0 navbar  min-h-0 flex-shrink-0 justify-between z-20 shadow-md shadow-secondary px-0 sm:px-2">
       <div className="navbar-start w-auto lg:w-1/2">
         <div className="lg:hidden dropdown" ref={burgerMenuRef}>
           <label
@@ -100,14 +100,14 @@ export const Header = () => {
         </div>
         <Link href="/" passHref className="hidden lg:flex items-center gap-2 ml-4 mr-6 shrink-0">
           <div className="flex relative w-10 h-10">
-            <Image alt="SE2 logo" className="cursor-pointer" fill src="/logo.svg" />
+            <Image alt="Voto logo" className="cursor-pointer rounded-md" fill src="/logo.svg" />
           </div>
           <div className="flex flex-col">
-            <span className="font-bold leading-tight">MACI Wrapper</span>
-            <span className="text-xs">Private Voting Starter Kit</span>
+            <span className="font-bold leading-tight">Voto.tech</span>
+            <span className="text-xs">Decentralized anonymous voting solution</span>
           </div>
         </Link>
-        <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">
+        <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2 justify-items-center">
           <HeaderMenuLinks />
         </ul>
       </div>
