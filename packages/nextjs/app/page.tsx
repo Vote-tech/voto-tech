402936c35e7d8e97ae6d6b22ef95a377dc4a6c59
Dovydas Musulas <dovydas.musulas@proton.me>
2024-05-26 06:00:48 +0200

    a

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 
 import Image from "next/image";
+import RegisterButton from "./_components/RegisterButton";
 // import { SafePasskeysDemo } from "./registration/SafePasskeysDemo";
 import type { NextPage } from "next";
 import HeroImage from "~~/assets/private_voting.png";
 import { useAuthUserOnly } from "~~/hooks/useAuthUserOnly";
-import RegisterButton from "./_components/RegisterButton";
 
 const Home: NextPage = () => {
   useAuthUserOnly({ inverted: true });
@@ -14,17 +14,14 @@ const Home: NextPage = () => {
     <>
       <div className="flex items-center flex-col flex-grow pt-10">
         <div className="px-5">
-          <h1 className="text-4xl font-bold text-center">
-            Vote with Voto
-          </h1>
+          <h1 className="text-4xl font-bold text-center">Vote with Voto</h1>
 
           <div className="flex flex-col-reverse md:flex-row justify-center items-center mt-10 sm:w-2/3 mx-auto gap-x-10 gap-y-5 mb-10">
             <div className="flex-1">
               <p className="text-lg mt-5 text-justify">
-                Now you finally vote with the Voto Voto.
-                Voto is an on-chain censorship-resistant, 
-                voting solution built on decentralised identities.
-
+                Now you finally vote with the Voto Voto. Voto is an on-chain
+                censorship-resistant, voting solution built on decentralised
+                identities.
               </p>
               <div className="text-center">
                 <RegisterButton />
